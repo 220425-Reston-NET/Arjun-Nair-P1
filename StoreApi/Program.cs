9b94151e15a3a8344b3f8e698b8fd7f5509d1bef
NairArjunWD <nair.arjunr94@gmail.com>
2022-06-07 22:36:04 -0700

    Testing to fix error

--- StoreApi/Program.cs
+++ StoreApi/Program.cs
@@ -15,10 +15,10 @@ builder.Services.AddScoped<IRepository<Customer>, SQLCustomerRepository>(repo =>
 builder.Services.AddScoped<ICustomerBL, CustomerBL>();
 builder.Services.AddScoped<IRepository<Products>, SQLProductRepository>(repo => new SQLProductRepository(Environment.GetEnvironmentVariable("Arjun_Nair")));
 builder.Services.AddScoped<IProductBL, ProductBL>();
-builder.Services.AddScoped<IRepository<Store>, SQLStoreRepository>(repo => new SQLStoreRepository(Environment.GetEnvironmentVariable("Arjun_Nair")));
-builder.Services.AddScoped<IStoresBL, StoresBL>();
-builder.Services.AddScoped<IRepository<ProductDescriptionJoin>, SQLProductDescriptionJoinRepo>(repo => new SQLProductDescriptionJoinRepo(Environment.GetEnvironmentVariable("Arjun_Nair")));
-builder.Services.AddScoped<IProdDescJoinBL, ProdDescJoinBL>();
+// builder.Services.AddScoped<IRepository<Store>, SQLStoreRepository>(repo => new SQLStoreRepository(Environment.GetEnvironmentVariable("Arjun_Nair")));
+// builder.Services.AddScoped<IStoresBL, StoresBL>();
+// builder.Services.AddScoped<IRepository<ProductDescriptionJoin>, SQLProductDescriptionJoinRepo>(repo => new SQLProductDescriptionJoinRepo(Environment.GetEnvironmentVariable("Arjun_Nair")));
+// builder.Services.AddScoped<IProdDescJoinBL, ProdDescJoinBL>();
 
 
 
